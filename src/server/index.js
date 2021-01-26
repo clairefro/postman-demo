c32a3218075048eacc3b96c9a0a73124b5403f43
Claire Froelich <claire.froelich@gmail.com>
2021-01-25 19:45:43 -0800

    Connect frontend to backend

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,17 @@
 const express = require("express");
-const os = require("os");
-
+const cors = require("cors");
 const app = express();
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3030;
 app.use(express.static("dist"));
-app.get("/api/getUsername", (req, res) => res.send({ username: os.userInfo().username }));
+
+app.use(
+	// cors({
+	// 	origin: "http://localhost:8080",
+	// })
+	cors()
+);
+
+app.get("/api/username", (req, res) => res.send({ username: "foo" }));
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
